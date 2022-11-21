5e0213d13a14feda251926c3bda3c1996b5ff1a3
Ubinquitous <ubinquitous1@gmail.com>
2022-11-21 19:42:57 +0900

    ADD : User findAll

--- Node JS/src/index.js
+++ Node JS/src/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const User = require('../database/models/user');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send(`Hello, this is index. now your path : ${__dirname}`);
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await User.findAll();
+        console.log(users)
+        res.send(users)
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 })
 
 module.exports = router;
